078ac11e1dc59b04b47ca695736d17570d537a5d
Imran Sayed <imranhsayed@gmail.com>
2019-01-17 13:21:52 +0530

    Wrap the the slider object in a self-executing anonymous function

--- javascript.js
+++ javascript.js
@@ -1,44 +1,53 @@
-var slider = {
-	/**
-	 * Function that initializes the other functions.
-	 *
-	 * @return {void}
-	 */
-	init: function () {
-		this.initializeVariables();
-		this.changeImg();
-	},
+( function () {
 
-	/**
-	 * Initializes variables.
-	 *
-	 * @return {void}
-	 */
-	initializeVariables: function () {
-		// Image List
-		this.counter = 0;
-		this.images = [];
+	"use strict";
 
-		this.images[0] = 'img/pic-1.png';
-		this.images[1] = 'img/pic-2.png';
-		this.images[2] = 'img/pic-3.png';
-	},
+	var slider = {
+		/**
+		 * Function that initializes the other functions.
+		 *
+		 * @return {void}
+		 */
+		init: function () {
+			this.initializeVariables();
+			this.changeImg();
+		},
 
-	/**
-	 * Changes images source every 3 secs, to create an image slider.
-	 *
-	 * @return {void}
-	 */
-	changeImg: function () {
-		this.slideEl = document.querySelector( '.slide' );
-		this.slideEl.src = this.images[ this.counter ];
-		if ( this.counter < ( this.images.length - 1 ) ) {
-			this.counter++;
-		} else {
+		/**
+		 * Initializes variables.
+		 *
+		 * @return {void}
+		 */
+		initializeVariables: function () {
+			// Image List
 			this.counter = 0;
-		}
-		setTimeout( 'slider.changeImg()', 3000 );
-	}
-};
+			this.images = [];
 
-slider.init();
+			this.images[0] = 'img/pic-1.png';
+			this.images[1] = 'img/pic-2.png';
+			this.images[2] = 'img/pic-3.png';
+		},
+
+		/**
+		 * Changes images source every 3 secs, to create an image slider.
+		 *
+		 * @return {void}
+		 */
+		changeImg: function () {
+			const that = this;
+
+			console.log( that );
+			this.slideEl = document.querySelector( '.slide' );
+			this.slideEl.src = this.images[ this.counter ];
+			if ( this.counter < ( this.images.length - 1 ) ) {
+				this.counter++;
+			} else {
+				this.counter = 0;
+			}
+			setTimeout( this.changeImg.bind( this ), 3000 );
+		}
+	};
+
+	slider.init();
+
+} )();
